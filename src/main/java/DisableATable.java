844735a4ac76c561423c8031baa82a2936656256
immrdg <d.gireesh21@gmail.com>
2023-03-22 10:26:19 +0530

    Disable Table In HBase Using Java Api

---
+++ src/main/java/DisableATable.java
@@ -0,0 +1,36 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import java.io.IOException;
+
+public class DisableATable {
+    public static void main(String[] args) throws IOException {
+
+        // Instantiating the Configuration class with HBase configuration
+        Configuration conf = HBaseConfiguration.create();
+
+        // Creating a Connection instance using the Configuration object
+        Connection connection = ConnectionFactory.createConnection(conf);
+
+        // Instantiating the Admin class to perform administrative operations
+        Admin admin = connection.getAdmin();
+
+        // I have employee table in my HBase.
+        // Table to Be Disabled
+        TableName empTable = TableName.valueOf("employee");
+
+        // Checking if the table is disabled before performing any operations
+        boolean isTableDisabled = admin.isTableDisabled(empTable);
+
+        System.out.println(isTableDisabled); //false;
+
+        if(!isTableDisabled){
+            admin.disableTable(empTable);
+        }
+        System.out.println(admin.isTableDisabled(empTable));  // true
+    }
+}
